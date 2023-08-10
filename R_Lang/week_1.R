2a9076cb8acf24acb0d7f196ea4fcbec4ac99b7d
Manoj Paudel <mpsandip12@gmail.com>
2023-08-10 07:18:57 +0545

    new commit

--- R_Lang/week_1.R
+++ R_Lang/week_1.R
@@ -18,3 +18,21 @@ dev.off() #to close the file that we created.
 
 # Shortcut to run- first to end - Cmd+opti+B
 
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+# Example - 2
+
+# Create data for the graph.
+x <- c(21, 62, 10, 53)
+labels <- c("London", "New York", "Singapore", "Mumbai")
+
+# Give the chart file a name.
+png(file = "city_title_colours.jpg")
+
+# Plot the chart with title and rainbow color pallet.
+pie(x, labels, main = "City pie chart", col = rainbow(length(x)))
+
+# Save the file.
+dev.off()
+
+
