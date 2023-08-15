aa55eb631088f6f1f7bf1003527c87f9fad67890
Manoj Paudel <mpsandip12@gmail.com>
2023-08-15 10:50:26 +0545

    update

--- R_Code/lab2_counting.R
+++ R_Code/lab2_counting.R
@@ -1,15 +1,38 @@
+
+#1.
 factorial(3) 
 
-
-
-perm = function(n,x){
-  factorial(n)/factorial(n-x)
+#2.
+perm = function(n,r){
+  factorial(n)/factorial(n-r)
 }
 
-perm(10,2)
+perm(5,4)
 
-comb = function(n,x){
-  factorial(n) / (factorial(n-x) * factorial(x))
+#3.
+comb = function(n,r){
+  factorial(n) / (factorial(n-r) * factorial(r))
 }
 
 comb(10,2)
+
+#4.
+
+install.packages('gtools')
+#load library
+library(gtools)
+#run with 3 balls
+x <- c('red', 'blue', 'black')
+#pick 2 balls from the urn with replacement
+#get all permutations
+permutations(n=3,r=2,v=x,repeats.allowed=T)
+# - n -> kati ota element
+# r -> number of row
+# repeats.allowed=T, F -> repeats the same permutation or not.
+
+#5.
+#number of permutations (rows)
+nrow(permutations(n=3,r=2,v=x,repeats.allowed=T))
+
+
+
