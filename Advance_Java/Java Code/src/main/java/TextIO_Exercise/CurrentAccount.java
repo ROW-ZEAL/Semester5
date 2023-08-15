ca7be03398e30c761399050fa5d0bf789a7cea85
Manoj Paudel <mpsandip12@gmail.com>
2023-08-15 07:20:46 +0545

    new commit

---
+++ Advance_Java/Java Code/src/main/java/TextIO_Exercise/CurrentAccount.java
@@ -0,0 +1,9 @@
+package TextIO_Exercise;
+
+public class CurrentAccount extends SuperBankClass {
+	double overdraftvalue;
+	public CurrentAccount(String acno, double acbal, double overdraft ) {
+		super(acno,acbal);
+		this.overdraftvalue = overdraft;
+	}
+}
