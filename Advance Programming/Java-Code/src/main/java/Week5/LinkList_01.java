d99ae28791876aa33c461ea35d1a7f23829830ba
Manoj Paudel <mpsandip12@gmail.com>
2023-08-27 12:07:45 +0545

    Update LinkList_01.java

--- Advance Programming/Java-Code/src/main/java/Week5/LinkList_01.java
+++ Advance Programming/Java-Code/src/main/java/Week5/LinkList_01.java
@@ -6,13 +6,24 @@ public class LinkList_01 {
     public static void main(String[] args) {
         LinkedList<String> li = new LinkedList<String>();
 
-        // Addd elements
+
+        // Add elements
+        // Can add First and Last - using addFirst, addLast
+
         li.add("A");
         li.add("B");
-        li.add("C");
-        li.add("D");
-        li.add("E");
+        li.addLast("C");
+        li.addFirst("D");
+        li.add(2, "E");
 
         System.out.println(li);
+
+        // remove
+        li.remove("B");
+        System.out.println(li);
+        li.remove(3); // index
+        System.out.println(li);
+
+
     }
 }
